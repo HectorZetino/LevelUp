ff6bb7565345b2e07e7d285901a3421466edf7f0
Hector Zetino <hrzetino@correo.url.edu.gt>
2022-10-26 15:00:06 -0600

    Visualizacion de los pokemones, pendiente colocacion con css

--- Proyecto No.4/app.js
+++ Proyecto No.4/app.js
@@ -1,10 +1,38 @@
 const content_pokemon = document.getElementById('Content_pokemon');
 const counter_pokemon = 150;
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+const fetchPoke = async () =>{
+    for(let i = 0; i < 6; i++){
+        const rPoke = getRandomInt(counter_pokemon);
+        await getPoke(rPoke);
+    }
+}
 const getPoke = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
-    console.log(pokemon);
+    createPokeCard(pokemon);
 }
-getPoke(1);
+
+const createPokeCard = (pokemon) => {
+    const elementPoke = document.createElement('div');
+    elementPoke.classList.add('pokemon');
+    const { id, name, sprites, types} = pokemon;
+    const type = types[0].type.name;
+    const pokeInnerHTML = `
+    <div class="img-container">
+        <img src="${sprites.front_default}" alt="${name}"/>
+    </div>
+    <div class="info">
+        <span class="number">${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Tipo: <span>${type}</span></small>
+    </div>
+    `;
+    elementPoke.innerHTML = pokeInnerHTML;
+    container_pokemon.appendChild(elementPoke);
+}
+fetchPoke();
